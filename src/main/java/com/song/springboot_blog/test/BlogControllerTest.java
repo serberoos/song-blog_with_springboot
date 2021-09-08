78244d9f2978f9faa65093876d898a695b53bd3c
JaeGeun.Song <jae99c@gmail.com>
2021-09-08 16:42:57 +0900

    start springboot_server and print hello spring boot

---
+++ src/main/java/com/song/springboot_blog/test/BlogControllerTest.java
@@ -0,0 +1,15 @@
+package com.song.springboot_blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 스프링이 com.song.spring_blog 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하는 것은 아니고
+// 특정 어노테이션이 붙어 있는 클래스 파일들을 new 해서 (IoC) 스프링 컨테이너에 관리 해준다.
+@RestController
+public class BlogControllerTest {
+	
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+}
