ee0683e875e6d81d8cb9a2e7a78ecc4d87708e42
JaeGeun.Song <jae99c@gmail.com>
2021-09-13 13:46:56 +0900

    Object Mapping Method @RequestParam and @RequestBody

--- src/main/java/com/song/springboot_blog/test/HttpControllerTest.java
+++ src/main/java/com/song/springboot_blog/test/HttpControllerTest.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // if Controller? 사용자가 요청 -> HTML 파일 
@@ -16,20 +18,21 @@ public class HttpControllerTest {
 	//인터넷 브라우저에서는 무조건 get요청밖에 할 수 없다.
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
-	public String getTest() {
-		return "get 요청";
+	public String getTest(Member m) {//MessageConverter (스프링 부트)
+		
+		return "get 요청 :" + m.getId() +", " + m.getUsername()+", " +m.getPassword()+", " +m.getEmail();
 	}
 	
 	//http://localhost:8080/http/post (insert)
-	@PostMapping("/http/post")
-	public String postTest() {
-		return  "post 요청";
+	@PostMapping("/http/post") // text/plain, application/json
+	public String postTest(@RequestBody Member m) { //MessageConverter (스프링 부트)
+		return "post 요청 :" + m.getId() +", " + m.getUsername()+", " +m.getPassword()+", " +m.getEmail();
 	}
 
 	//http://localhost:8080/http/put (update)
 	@PutMapping("/http/put")
-	public String putTest() {
-		return  "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 :" + m.getId() +", " + m.getUsername()+", " +m.getPassword()+", " +m.getEmail();
 	}
 	
 	//http://localhost:8080/http/delete (delete)
