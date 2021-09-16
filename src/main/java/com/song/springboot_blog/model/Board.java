8016015295fe5402f08c3719c8da74fb7b009a40
JaeGeun.Song <jae99c@gmail.com>
2021-09-16 14:17:09 +0900

    Organize the relationship

--- src/main/java/com/song/springboot_blog/model/Board.java
+++ src/main/java/com/song/springboot_blog/model/Board.java
@@ -4,16 +4,19 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -41,9 +44,16 @@ public class Board {
 	@ColumnDefault("0") //int이므로 홀따옴표를 쓰지 않는다. String일 경우 DB에 알려주기 위해 홀 따옴표 사용
 	private int count; // 조회수
 	
-	@ManyToOne // Board = Many, User = One 한명의 유저는 여러 게시글을 쓸 수 있다. | 연관관계
+	// Board = Many, User = One 한명의 유저는 여러 게시글을 쓸 수 있다. | 연관관계
+	@ManyToOne(fetch = FetchType.EAGER) //Many = Many, User = One // EAGER 전략 : 무조건 얘 들고와
 	@JoinColumn(name="userId") // 필드값은 One으로 만들어지고 연관관계는 Many To로 만들어짐
 	private User user; //ORM에서는 키값으로 찾지 않고 오브젝트로 사용 (DB는 오브젝트를 사용 불가 FK 사용) => 여기서 자바와 DB가 충돌남 => 맞춰서 테이블을 만들게됨.ORM이 바꿈.
+	
+	 //한개의 게시글에 여러 댓글이 존재할 수 있다.
+	@OneToMany(mappedBy="board", fetch = FetchType.EAGER) // mappedBy가 적혀있으면 연관관계의 주인이 아니다(난 FK가 아니에요) DB에 칼럼을 만들지 마세요. 
+	// LAZY 전략 필요할 때 들고 와 하지만 Board들고 올때 다른것도 다 들고와야 되기 때문에 EAGER 전략으로 바꾸어주어야 한다. fetch = FetchType.LAZY => EAGER
+	//@JoinColumn(name="replyId") FK는 DB에 있어야 한다. 원자성이 깨짐
+	private List<Reply> reply; //board를 select할 때, FK를 이용해서 값을 얻기 위한 것이다.
 	
 	@CreationTimestamp
 	private Timestamp createDate;
---
+++ src/main/java/com/song/springboot_blog/test/DummyControllerTest.java
@@ -0,0 +1,20 @@
+package com.song.springboot_blog.test;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyControllerTest {
+	
+	
+	//http://localhost:8000/blog/dummy/join (요청)
+	//http의 body에 username, password, email 데이터를 가지고 요청하게 되면...
+	@PostMapping("/dummy/join")
+	public String join(String username, String password, String email) {
+		System.out.println("username"+username);
+		System.out.println("password"+password);
+		System.out.println("email"+email);
+		return "회원가입이 완료되었습니다.";
+		
+	}
+}
