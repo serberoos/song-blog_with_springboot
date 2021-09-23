5974534e5c1284f77aa182006c6f60238bbf7336
JaeGeun.Song <jae99c@gmail.com>
2021-09-23 17:25:16 +0900

    * select user data from DB and deal with exception * change ddl-auto: create -> ddl-auto: update

--- src/main/java/com/song/springboot_blog/test/DummyControllerTest.java
+++ src/main/java/com/song/springboot_blog/test/DummyControllerTest.java
@@ -1,6 +1,11 @@
 package com.song.springboot_blog.test;
 
+
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +18,37 @@ public class DummyControllerTest {
 	
 	@Autowired //DummyControllerTest가 메모리에 뜰때, 같이 뜬다. (의존성 주입)DI
 	private UserRepository userRepository;
+	
+	// {id} 주소로 파라미터를 전달 받을 수 있다.
+	// http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+// user /4번을 찾으면 내가 DB에서 못찾으면 user가 null이 되니까 Optional로 User객체를 감싸서 가져올 테니 null인지 아닌지 판단해서 리턴할 것
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+//
+//			@Override
+//			public User get() {
+//				// TODO Auto-generated method stub
+//				return new User();
+//			}
+//		});
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() { //User를 넣을 경우 전부 null이 리턴됨
+
+			@Override
+			public IllegalArgumentException get() {
+				// TODO Auto-generated method stub
+				return new IllegalArgumentException("해당 유저는 없습니다 id :" + id); //람다식으로 하면 더 편할 수 있다.
+			}
+		});
+		// 요청 : 웹 브라우저
+		// user 객체 = 자바 오브젝트
+		// 변환 (웹브라우저가 이해할 수 있는 데이터 -> json (Gson 라이브러리)
+		// 스프링 부트 = MessageConverter라는 애가 응답시에 자동 작동
+		// 만약에 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson 라이브러리를 호출해서
+		// user 오브젝트를 json으로 변환해서 브라우저에게 던져줍니다.
+		return user;
+	}
+
 	//http://localhost:8000/blog/dummy/join (요청)
 	//http의 body에 username, password, email 데이터를 가지고 요청하게 되면...
 	@PostMapping("/dummy/join") //key=value(약속된 규칙 : x-www-form-urlencoded
