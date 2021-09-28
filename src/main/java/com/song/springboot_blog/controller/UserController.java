26d7b92bf3cab8bebd5633791cf747b104f70d5e
JaeGeun.Song <jae99c@gmail.com>
2021-09-28 14:30:08 +0900

    Create login/join Template Layout

--- src/main/java/com/song/springboot_blog/controller/UserController.java
+++ src/main/java/com/song/springboot_blog/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.song.springboot_blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+	@GetMapping("/user/joinForm")
+	public String joinForm() {
+		
+		return "user/joinForm";
+	}
 	
+	@GetMapping("/user/loginForm")
+	public String loginForm() {
+		
+		return "user/loginForm";
+	}
 }
