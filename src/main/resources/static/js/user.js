a620d3cdecf27bfe218b6b64d608405a43162e0e
JaeGeun.Song <jae99c@gmail.com>
2021-09-30 14:49:52 +0900

    Change request URL before implementation of spring security

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -25,7 +25,7 @@ let index = {
 		$.ajax({
 			// 회원가입 수행 요청
 			type:"POST",
-			url:"/blog/api/user",
+			url:"/api/user",
 			data: JSON.stringify(data), //JSON 스트링으로 변경해 데이터를 전송한다. / http body 데이터 : 이 경우 MINE 타입이 필요해진다.
 			contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지(MINE)
 			//dataType: "json" //요청을 서버로 해서 응답이 왔을 때, 기본적으로 모든 것이 문자열 (생긴게 json이라면?) => javaScript 오브젝트로 변경
@@ -33,7 +33,7 @@ let index = {
 			
 			alert("회원가입이 완료 되었습니다.");
 			console.log(resp);
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));	
 		}); 
@@ -48,7 +48,7 @@ let index = {
 		$.ajax({
 			// 회원가입 수행 요청
 			type:"POST",
-			url:"/blog/api/user/login",
+			url:"api/user/login",
 			data: JSON.stringify(data), //JSON 스트링으로 변경해 데이터를 전송한다. / http body 데이터 : 이 경우 MINE 타입이 필요해진다.
 			contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지(MINE)
 			//dataType: "json" //요청을 서버로 해서 응답이 왔을 때, 기본적으로 모든 것이 문자열 (생긴게 json이라면?) => javaScript 오브젝트로 변경
@@ -56,7 +56,7 @@ let index = {
 			
 			alert("로그인이 완료 되었습니다.");
 			console.log(resp);
-			location.href = "/blog";
+			location.href = "/";
 		}).fail(function(error){
 			alert(JSON.stringify(error));	
 		}); 
